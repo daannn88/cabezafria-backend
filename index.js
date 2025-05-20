548bcea9400db1e835db18e89c4c42f5f4f3d35f
daannn88 <celydaniel97@gmail.com>
2025-05-19 19:01:30 -0500

    [+]Se realizó una prueba

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 // const express = require('express');             //commonJS
-import express from 'express';
+//Paso 1:Importamos dependecias
+import express from 'express';                      //ESModule
 
+//Paso 2: Ejecutamos Express
 const app = express();              //Invocación de express
 
+//Paso 3: Crear los EndPoints (Puntos de entrada)
+app.get('/', (req, res)=>{
+    res.send('<h1>Home</h1>')
+});
+
+app.get('/about-us', (req, res)=>{
+    res.send('<h1>About us</h1>')
+});
+
+//Paso 4: Lanzamos el servidor web usando express escuchando 
 app.listen(3000, ()=>{
-    console.log('Servidor lanzado exitosamente! :)')
+    console.log('Servidor lanzado exitosamentee! :)')
 });
