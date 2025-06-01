8f451923269d71b1b187aeb93b33c186e787ecb5
daannn88 <celydaniel97@gmail.com>
2025-05-31 21:18:37 -0500

    [+] auth.route: define POST /api/register

--- src/routes/auth.route.mjs
+++ src/routes/auth.route.mjs
@@ -1,5 +1,8 @@
 import express from 'express'
+import { createUsers } from '../controllers/users.controllers.mjs'
 
 const router = express.Router()
+
+router.post('/api/login', createUsers);
 
 export default router
