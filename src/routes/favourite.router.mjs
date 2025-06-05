3d13e5b047f52382efc9194b4fe7d2608e204328
felipe <felipez111946@gmail.com>
2025-06-04 21:19:13 -0500

    Se vincula category.Schema con product.Schema mediante el Id

--- src/routes/favourite.router.mjs
+++ src/routes/favourite.router.mjs
@@ -1,5 +1,5 @@
 import express from "express";
-import { createfavourite } from "../controllers/fovourite.controller.mjs";
+import { createfavourite } from "../controllers/favourite.controller.mjs";
 
 const router = express.Router();
 
